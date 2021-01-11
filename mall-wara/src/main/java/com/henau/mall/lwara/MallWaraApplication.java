c26d79ce409113cc5693e59ba9abc6256b412c7c
lijianming <64688425+shiljm@users.noreply.github.com>
2021-01-11 17:47:19 +0800

    项目初始结构创建

---
+++ mall-wara/src/main/java/com/henau/mall/lwara/MallWaraApplication.java
@@ -0,0 +1,13 @@
+package com.henau.mall.lwara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallWaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallWaraApplication.class, args);
+    }
+
+}
---
+++ mall-wara/src/test/java/com/henau/mall/lwara/MallWaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henau.mall.lwara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWaraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
