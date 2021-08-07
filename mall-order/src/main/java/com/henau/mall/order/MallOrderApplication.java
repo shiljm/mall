d1b3aaaac37bf4d9b1ef9f54fb32154ab5262ec8
LJM <2454951743@qq.com>
2021-08-07 11:07:33 +0800

    更新

--- mall-order/src/main/java/com/henau/mall/order/MallOrderApplication.java
+++ mall-order/src/main/java/com/henau/mall/order/MallOrderApplication.java
@@ -1,9 +1,23 @@
 package com.henau.mall.order;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 使用RabbitMQ
+ * 1、引入amqp场景
+ * 2、给容器中自动配置了
+ *      RabbitTemplate、AmqpAdmin、CachingConnectionFactory、RabbitMessagingTemplate
+ *      所有的属性都是spring.rebbitmq
+ *      @ConfigurationProperties(prefix = "spring.rabbitmq")
+ *      public class RabbitProperties
+ *
+ * 3、给配置文件中配置 spring.rabbitmq 信息
+ * 4、@EnableRabbit:@EnableXxxxx
+ */
+@EnableRabbit
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MallOrderApplication {
--- mall-order/src/test/java/com/henau/mall/order/MallOrderApplicationTests.java
+++ mall-order/src/test/java/com/henau/mall/order/MallOrderApplicationTests.java
@@ -1,13 +1,47 @@
 package com.henau.mall.order;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class MallOrderApplicationTests {
+@Slf4j
+public class MallOrderApplicationTests {
+
+    @Autowired
+    AmqpAdmin amqpAdmin;
+
+    /**
+     * 1、如何创建Exchange、Queue、Binding
+     *      1)、使用AmqpAdmin进行创建
+     * 2、如何收发消息
+     */
 
     @Test
-    void contextLoads() {
+    public void createExchange() {
+        //amqpAdmin
+        //Exchange
+        DirectExchange directExchange = new DirectExchange("hello-java-exchange", true, false);
+        amqpAdmin.declareExchange(directExchange);
+        log.info("Exchange[{}]创建成功", "hello-java-exchange");
+    }
+
+    @Test
+    public void createQueue() {
+
+        Queue queue = new Queue();
+        amqpAdmin.declareQueue(queue);
+        log.info("Queue[{]}创建成功", "hello-java-queue");
     }
 
 }
